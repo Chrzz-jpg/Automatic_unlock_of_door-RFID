8629fd05021943db1d13d92a5b409609cdb51789
boottD <chrisroos14@gmail.com>
2019-07-16 06:06:48 -0300

    Front uses - ideias - estrutura de controllers

---
+++ backend/controllers/logcontroller.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\v1\Controllers;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class LogController {
+
+    /**
+     * Container - Ele recebe uma instancia de um 
+     * container da rota no construtor
+     * @var object s
+     */
+
+   
+   protected $tag;
+   protected $matricula;
+   protected $name;
+
+   public function __construct($tag, $name, $matricula) {
+       $this->$tag = $tag;
+       $this->$name = $name;
+       $this->$matricula = $matricula;
+       }
+
+    public function searchAll()
+    {
+        # code...
+    }
+
+}
