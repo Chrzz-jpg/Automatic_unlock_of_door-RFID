3c003b43f485a04ae16214d7c19ea76049bfef8e
juaryR <jrochamindelo@gmail.com>
2019-07-16 23:51:02 -0300

    Backend algumas alterações

--- backend/controllers/EspController.php
+++ backend/controllers/EspController.php
@@ -3,34 +3,82 @@
 namespace App\Controllers;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Doctrine\DBAL\Types\DateTimeType;
+use Entity\Logs;
 
 class EspController {
 
+  
     /**
-     * Container - Ele recebe uma instancia de um 
-     * container da rota no construtor
-     * @var object s
-     */
+   * Container Class
+   * @var [object]
+   */
+  private $container;
 
-   
-   protected $tag;
-   protected $matricula;
-   protected $name;
+  /**
+   * Undocumented function
+   * @param [object] $container
+   */
+  public function __construct($container)
+  {
+    $this->container = $container;
+  }
 
-   public function __construct($tag, $name, $matricula) {
-       $this->$tag = $tag;
-       $this->$name = $name;
-       $this->$matricula = $matricula;
-       }
-
-    public function autenticUser($tag)
+  public function validarUser($tag)
     {
-        # code...
+        $entityManager = $this->container->get('em');
+        $userRepo = $entityManager->getRepository('Entity\Users');
+        $user = $userRepo->findOneBy([
+            "tagId" => $tag,
+            ]); 
+
+        return $user ;
+    }
+
+    /**
+   * Método de Exemplo
+   *
+   * @param [type] $request
+   * @param [type] $response
+   * @param [type] $args
+   * @return void Response
+   */
+    public function autenticUser($request, $response, $args)
+    {
+        $acess = false;
+        $code  = 404;
+
+        $params = (object) $request->getParams();
+        $TagId = (string) $params->tagId;
+
+          $user = $this->validarUser($TagId);
+        if ($user) {
+  
+            $log = new Logs();
+            $entityManager = $this->container->get('em');
+            $log->setMatricula($user);
+            $log->setIndate(new \DateTime(date("Y-m-d H:i:s")));
+        
+            $entityManager->persist($log);
+            $entityManager->flush();
+
+            $acess = true;
+            $code = 200 ;
+
+        }
+        
+        $return = $response->withJson(['acess' => $acess], $code)
+        ->withHeader('Content-type', 'application/json')
+        ->withHeader("Access-Control-Allow-Origin", "*");
+        return $return; 
+
     }
 
     public function registerLogTable($tag, $name, $matricula)
     {
         # code...
     }
+
+    
 
 }
--- backend/controllers/LogController.php
+++ backend/controllers/LogController.php
@@ -1,30 +1,50 @@
 <?php
-// namespace App\Controllers;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+namespace App\Controllers;
 
 class LogController {
 
     /**
-     * Container - Ele recebe uma instancia de um 
-     * container da rota no construtor
-     * @var object s
-     */
+   * Container Class
+   * @var [object] 
+   */
+  private $container;
 
-   
-   protected $tag;
-   protected $matricula;
-   protected $name;
+  /**
+   * Undocumented function
+   * @param [object] $container
+   */
+  public function __construct($container)
+  {
+    $this->container = $container;
+  }
 
-   public function __construct($tag, $name, $matricula) {
-       $this->$tag = $tag;
-       $this->$name = $name;
-       $this->$matricula = $matricula;
-       }
 
-    public function searchAll()
-    {
-        # code...
+    /**
+   * Método de Exemplo
+   *
+   * @param [type] $request
+   * @param [type] $response
+   * @param [type] $args
+   * @return void Response
+   */
+  public function searchAll($request, $response, $args)
+  {
+    $entityManager = $this->container->get('em');
+    $logRepo = $entityManager->getRepository('Entity\Logs');
+
+    $logs = $logRepo->findAll(); 
+    $uses_array = array();
+    foreach ($logs as $log) {
+        $uses_array[] = array(
+            'name' => $log->getMatricula()->getNome(),
+            'id' => $log->getMatricula()->getId(),
+            'data' => $log->GetIndate()->format("Y-m-d H:i:s"),
+            // other fields
+        );
     }
+
+    return $response->withJson($uses_array)
+      ->withHeader("Access-Control-Allow-Origin", "*");
+  }
 
 }
