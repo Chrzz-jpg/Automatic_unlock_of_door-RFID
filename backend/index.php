bf1950f03776f72f51579d58b7b277c14d66aacd
juaryR <jrochamindelo@gmail.com>
2019-07-18 23:18:46 -0300

    clean the code

--- backend/index.php
+++ backend/index.php
@@ -6,27 +6,4 @@ require 'bootstrap.php';
  */
 require 'routes.php';
 
-$app->get('/', function ($request, $response) {
-
-    $a->name = "Christian";
-    $a->id = 17204298;
-    $a->data = "Passou em POO";
-
-    $b->name = "Cristian";
-    $b->id = 14011234;
-    $b->data = "Nunca mais faz a REC";
-
-
-    // $dataArray = array(['id' => '12', 'name' => 'somethingElse', 'data' => '00']);
-    $dataArray = [];
-
-    array_push ( $dataArray  ,$a);
-    array_push ( $dataArray  ,$b);
-
-    // return $response->write(json_encode($dataArray))
-    return $response->withJson($dataArray)
-    ->withHeader("Access-Control-Allow-Origin","*");
-});
-
-
 $app->run();
